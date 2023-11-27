fef83ce8dd819398e4ee8b771718641eccdb77e4
Gobinda Das <144037976+gdeditzyt@users.noreply.github.com>
2023-11-27 11:14:38 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// let box = document.querySelector(".img-div");
+
+// box.addEventListener("mouseover", ()=> {
+//     gsap.from(".img-div h1, .img-div h2", {
+//         opacity: 0,
+//         duration: 1,
+//         lineHeight: 0,
+//         delay: 0.5,
+//     })
+// })
+
+// box.addEventListener("mouseout", ()=> {
+//     document.querySelector(".img-div h1, .img-div h2").style.opacity = "0";
+//     document.querySelector(".img-div h2").style.opacity = "0";
+// })
+
